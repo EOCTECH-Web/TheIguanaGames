e7336760b039d37d37777ccd6392f0782865cb24
Allen Findley <Allen.w.findley@eoctech.org>
2019-02-01 08:53:58 -0600

    renamed some of the files

--- leagueTournySite/js/style.js
+++ leagueTournySite/js/style.js
@@ -1,7 +1,8 @@
  var pw1 = document.getElementsByClassName("pw1").value
  var pw2 = document.getElementsByClassName("pw2").value
 
-$(".pwchec")
+$("checkPw").click(function() {
     if (pw1 != pw2) {
         alert("Passwords entered must match.")
     }
+})
