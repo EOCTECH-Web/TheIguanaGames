6095a8522c453452555836896883e75f8335e7f2
Allen Findley <Allen.w.findley@eoctech.org>
2019-02-07 14:20:57 -0600

    Switched ideas for the form
    old version of form is the formNonSimple files

--- Website/js/style.js
+++ Website/js/style.js
@@ -18,42 +18,71 @@ $(document).ready(function() {
         $("#add").css("display", "none")
         $("#current").css("display", "block")
     });
-    // mark the main position as the selected one
+    
+    // mark the main position as the selected one || The value will be changed to one to signify the selection of that option
+    
     $(".topMain").click(function() {
-        $(".topMain").css("background-color", "#da6134")
+        $(".topMain").css("background-color", "#004f67ff")
+        $(".topMain").css("color", "white")
         $(".jungleMain").css("background-color", "")
+        $(".jungleMain").css("color", "black")
         $(".midMain").css("background-color", "")
+        $(".midMain").css("color", "black")
         $(".adcMain").css("background-color", "")
+        $(".adcMain").css("color", "black")
         $(".supportMain").css("background-color", "")
+        $(".supportMain").css("color", "black")
     });
     $(".jungleMain").click(function() {
         $(".topMain").css("background-color", "")
-        $(".jungleMain").css("background-color", "#da6134")
+        $(".topMain").css("color", "black")
+        $(".jungleMain").css("background-color", "#004f67ff")
+        $(".jungleMain").css("color", "white")
         $(".midMain").css("background-color", "")
+        $(".midMain").css("color", "black")
         $(".adcMain").css("background-color", "")
+        $(".adcMain").css("color", "black")
         $(".supportMain").css("background-color", "")
+        $(".supportMain").css("color", "black")
     });
     $(".midMain").click(function() {
         $(".topMain").css("background-color", "")
+        $(".topMain").css("color", "black")
         $(".jungleMain").css("background-color", "")
-        $(".midMain").css("background-color", "#da6134")
+        $(".jungleMain").css("color", "black")
+        $(".midMain").css("background-color", "#004f67ff")
+        $(".midMain").css("color", "white")
         $(".adcMain").css("background-color", "")
+        $(".adcMain").css("color", "black")
         $(".supportMain").css("background-color", "")
+        $(".supportMain").css("color", "black")
     });
     $(".adcMain").click(function() {
         $(".topMain").css("background-color", "")
+        $(".topMain").css("color", "black")
         $(".jungleMain").css("background-color", "")
+        $(".jungleMain").css("color", "black")
         $(".midMain").css("background-color", "")
-        $(".adcMain").css("background-color", "#da6134")
+        $(".midMain").css("color", "black")
+        $(".adcMain").css("background-color", "#004f67ff")
+        $(".adcMain").css("color", "white")
         $(".supportMain").css("background-color", "")
+        $(".supportMain").css("color", "black")
     });
     $(".supportMain").click(function() {
         $(".topMain").css("background-color", "")
+        $(".topMain").css("color", "black")
         $(".jungleMain").css("background-color", "")
+        $(".jungleMain").css("color", "black")
         $(".midMain").css("background-color", "")
+        $(".midMain").css("color", "black")
         $(".adcMain").css("background-color", "")
-        $(".supportMain").css("background-color", "#da6134")
+        $(".adcMain").css("color", "black")
+        $(".supportMain").css("background-color", "#004f67ff")
+        $(".supportMain").css("color", "white")
     });
+
+
 
     // check to see if user agrees to liability waiver or not
     $(".agree").click(function() {
