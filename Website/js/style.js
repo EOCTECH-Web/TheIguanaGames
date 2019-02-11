6952f6a4fd0860f282a76e7fabab855943b4358b
IguanaGames <>
2019-02-11 09:07:03 -0600

    Merge hath been breaking everything

--- Website/js/style.js
+++ Website/js/style.js
@@ -24,22 +24,16 @@ $(document).ready(function() {
 
     $(".right").click(function() {
         $("#placeholder").hide()
-<<<<<<< HEAD
         $("#current").css("display", "none")
         $("#add").css("display", "block")
         $(".free").css("background-color", "#fff")
         $(".old").css("background-color", "#fff")
         $(".add").css("background-color", "#A1D7DA")
-=======
-        $("#add").css("display", "none")
-        $("#current").css("display", "block")
->>>>>>> fa4ed7af83eb3df930ae4aba9b9b9dc13e540c16
     });
 
     // mark the main position as the selected one || The value will be changed to one to signify the selection of that option
     
     $(".topMain").click(function() {
-<<<<<<< HEAD
         $(".topMain").css("background-color", "#A1D7DA")
         $(".topMain").val(1)
         $(".jungleMain").css("background-color", "")
@@ -74,42 +68,6 @@ $(document).ready(function() {
         $(".adcMain").val(0)
         $(".supportMain").css("background-color", "")
         $(".supportMain").val(0)
-=======
-        $(".topMain").css("background-color", "#004f67ff")
-        $(".topMain").css("color", "white")
-        $(".jungleMain").css("background-color", "")
-        $(".jungleMain").css("color", "black")
-        $(".midMain").css("background-color", "")
-        $(".midMain").css("color", "black")
-        $(".adcMain").css("background-color", "")
-        $(".adcMain").css("color", "black")
-        $(".supportMain").css("background-color", "")
-        $(".supportMain").css("color", "black")
-    });
-    $(".jungleMain").click(function() {
-        $(".topMain").css("background-color", "")
-        $(".topMain").css("color", "black")
-        $(".jungleMain").css("background-color", "#004f67ff")
-        $(".jungleMain").css("color", "white")
-        $(".midMain").css("background-color", "")
-        $(".midMain").css("color", "black")
-        $(".adcMain").css("background-color", "")
-        $(".adcMain").css("color", "black")
-        $(".supportMain").css("background-color", "")
-        $(".supportMain").css("color", "black")
-    });
-    $(".midMain").click(function() {
-        $(".topMain").css("background-color", "")
-        $(".topMain").css("color", "black")
-        $(".jungleMain").css("background-color", "")
-        $(".jungleMain").css("color", "black")
-        $(".midMain").css("background-color", "#004f67ff")
-        $(".midMain").css("color", "white")
-        $(".adcMain").css("background-color", "")
-        $(".adcMain").css("color", "black")
-        $(".supportMain").css("background-color", "")
-        $(".supportMain").css("color", "black")
->>>>>>> fa4ed7af83eb3df930ae4aba9b9b9dc13e540c16
     });
     $(".adcMain").click(function() {
         $(".topMain").css("background-color", "")
@@ -136,7 +94,7 @@ $(document).ready(function() {
         $(".supportMain").css("color", "white")
     });
 
-
+    
 
     // check to see if user agrees to liability waiver or not
     $(".agree").click(function() {
@@ -155,7 +113,6 @@ $(document).ready(function() {
             $("form").css("padding-bottom", "3.5%")
         }
     });
-<<<<<<< HEAD
 
 
 
@@ -194,7 +151,5 @@ $(document).ready(function() {
         $("#freeHidden").slideUp();
         $(".team_choice").slideUp();
     });
-=======
->>>>>>> fa4ed7af83eb3df930ae4aba9b9b9dc13e540c16
     
 });
