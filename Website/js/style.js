f1e51af6b6ae6b3dec9f3f89b913ac69e7c51c81
IguanaGames <>
2019-02-13 10:51:57 -0600

    Fixed the stuff

--- Website/js/style.js
+++ Website/js/style.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     // Added This (ttracy)
     $(".leagueForm").hide();
-    $("#freeHidden").hide();
+    $(".freeHidden").hide();
     $(".team_choice").hide();
     $(".cont_newteam").hide();
     $(".smashForm").hide();
@@ -14,201 +14,230 @@ $(document).ready(function() {
         if (value == "league") {
             $(".leagueForm").slideDown();
             $(".smashForm").slideUp();
+            $(".smashNewTeam").slideUp();
+            $(".smashTeamChoice").slideUp();
+            $(".smashFree").css("background-color", "#efefef")
+            $(".smashFree").css("color", "#004f67ff")
+            $(".smashOld").css("background-color", "#efefef")
+            $(".smashOld").css("color", "#004F67")
+            $(".smashAdd").css("background-color", "#efefef")
+            $(".smashAdd").css("color", "#004F67")
         }
         if (value == "smash") {
             $(".leagueForm").slideUp();
             $(".smashForm").slideDown();
+            $(".cont_newteam").slideUp();
+            $(".freeHidden").slideUp();
+            $(".team_choice").slideUp();
+            $(".free").css("background-color", "#efefef")
+            $(".old").css("background-color", "#efefef")
+            $(".add").css("background-color", "#efefef")
+            $(".topMain").css("background-color", "")
+            $(".jungleMain").css("background-color", "")
+            $(".midMain").css("background-color", "")
+            $(".adcMain").css("background-color", "")
+            $(".supportMain").css("background-color", "")
+            $(".topSub").css("background-color", "")
+            $(".jungleSub").css("background-color", "")
+            $(".midSub").css("background-color", "")
+            $(".adcSub").css("background-color", "")
+            $(".supportSub").css("background-color", "")
         }
         if (value == ""){
             $(".leagueForm").slideUp();
             $(".smashForm").slideUp();
+            $(".smashNewTeam").slideUp();
+            $(".smashTeamChoice").slideUp();
+            $(".smashFree").css("background-color", "#efefef")
+            $(".smashFree").css("color", "#004f67ff")
+            $(".smashOld").css("background-color", "#efefef")
+            $(".smashOld").css("color", "#004F67")
+            $(".smashAdd").css("background-color", "#efefef")
+            $(".smashAdd").css("color", "#004F67")
+            $(".cont_newteam").slideUp();
+            $(".freeHidden").slideUp();
+            $(".team_choice").slideUp();
+            $(".free").css("background-color", "#efefef")
+            $(".old").css("background-color", "#efefef")
+            $(".add").css("background-color", "#efefef")
+            $(".topMain").css("background-color", "")
+            $(".jungleMain").css("background-color", "")
+            $(".midMain").css("background-color", "")
+            $(".adcMain").css("background-color", "")
+            $(".supportMain").css("background-color", "")
+            $(".topSub").css("background-color", "")
+            $(".jungleSub").css("background-color", "")
+            $(".midSub").css("background-color", "")
+            $(".adcSub").css("background-color", "")
+            $(".supportSub").css("background-color", "")
         }
     });
     
+    // when user clicks on "Free agent"
+    $(".free").click(function() {
+        $(".freeHidden").slideDown();
+        $(".team_choice").slideUp();
+        $(".cont_newteam").slideUp();
+        $(".free").css("background-color", "#EDC164")
+        $(".old").css("background-color", "#efefef")
+        $(".add").css("background-color", "#efefef")
+
+    });
+
+    // when user clicks on "Already Have a Team"
+    $(".old").click(function() {
+        $(".team_choice").slideDown();
+        $(".freeHidden").slideUp();
+        $(".cont_newteam").slideUp();
+        $(".free").css("background-color", "#efefef")
+        $(".old").css("background-color", "#EDC164")
+        $(".add").css("background-color", "#efefef")
+    });
+
+    // when user clicks on "Add New Team"
+    $(".add").click(function() {
+        $(".cont_newteam").slideDown();
+        $(".freeHidden").slideUp();
+        $(".team_choice").slideUp();
+        $(".free").css("background-color", "#efefef")
+        $(".old").css("background-color", "#efefef")
+        $(".add").css("background-color", "#EDC164")
+    });
+
     // This is the league drop down scripts
 
-    $(".free").click(function() {
+    $(".smashFree").click(function() {
         $(".smashNewTeam").slideUp();
         $(".smashTeamChoice").slideUp();
-        $(".free").css("background-color", "#A1D7DA")
-        $(".free").css("color", "#004f67ff")
-        $(".old").css("background-color", "#fff")
-        $(".old").css("color", "black")
-        $(".add").css("background-color", "#fff")
-        $(".add").css("color", "black")
+        $(".smashFree").css("background-color", "#EDC164")
+        $(".smashFree").css("color", "#004f67ff")
+        $(".smashOld").css("background-color", "#efefef")
+        $(".smashOld").css("color", "#004F67")
+        $(".smashAdd").css("background-color", "#efefef")
+        $(".smashAdd").css("color", "#004F67")
     });
 
-    $(".old").click(function() {
+    $(".smashOld").click(function() {
         $(".smashNewTeam").slideUp();
         $(".smashTeamChoice").slideDown();
-        $(".free").css("background-color", "#fff")
-        $(".free").css("color", "black")
-        $(".old").css("background-color", "#A1D7DA")
-        $(".old").css("color", "#004f67ff")
-        $(".add").css("background-color", "#fff")
-        $(".add").css("color", "black")
+        $(".smashFree").css("background-color", "#efefef")
+        $(".smashFree").css("color", "#004F67")
+        $(".smashOld").css("background-color", "#EDC164")
+        $(".smashOld").css("color", "#004f67ff")
+        $(".smashAdd").css("background-color", "#efefef")
+        $(".smashAdd").css("color", "#004F67")
     });
 
-    $(".add").click(function() {
+    $(".smashAdd").click(function() {
         $(".smashNewTeam").slideDown();
         $(".smashTeamChoice").slideUp();
-        $(".free").css("background-color", "#fff")
-        $(".free").css("color", "black")
-        $(".old").css("background-color", "#fff")
-        $(".old").css("color", "black")
-        $(".add").css("background-color", "#A1D7DA")
-        $(".add").css("color", "#004f67ff")
+        $(".smashFree").css("background-color", "#efefef")
+        $(".smashFree").css("color", "#004F67")
+        $(".smashOld").css("background-color", "#efefef")
+        $(".smashOld").css("color", "#004F67")
+        $(".smashAdd").css("background-color", "#EDC164")
+        $(".smashAdd").css("color", "#004f67ff")
     });
 
     // mark the main position as the selected one || The value will be changed to one to signify the selection of that option
     
     $(".topMain").click(function() {
-        $(".topMain").css("background-color", "#A1D7DA")
+        $(".topMain").css("background-color", "#EDC164")
         $(".topMain").val(1)
-        $(".topMain").css("color", "#004f67ff")
         $(".jungleMain").css("background-color", "")
         $(".jungleMain").val(0)
-        $(".jungleMain").css("color", "black")
         $(".midMain").css("background-color", "")
         $(".midMain").val(0)
-        $(".midMain").css("color", "black")
         $(".adcMain").css("background-color", "")
         $(".adcMain").val(0)
-        $(".adcMain").css("color", "black")
         $(".supportMain").css("background-color", "")
         $(".supportMain").val(0)
-        $(".supportMain").css("color", "black")
     });
     $(".jungleMain").click(function() {
         $(".topMain").css("background-color", "")
         $(".topMain").val(0)
-        $(".topMain").css("color", "black")
-        $(".jungleMain").css("background-color", "#A1D7DA")
+        $(".jungleMain").css("background-color", "#EDC164")
         $(".jungleMain").val(1)
-        $(".jungleMain").css("color", "#004f67ff")
         $(".midMain").css("background-color", "")
         $(".midMain").val(0)
-        $(".midMain").css("color", "black")
         $(".adcMain").css("background-color", "")
         $(".adcMain").val(0)
-        $(".adcMain").css("color", "black")
         $(".supportMain").css("background-color", "")
         $(".supportMain").val(0)
-        $(".supportMain").css("color", "black")
     });
     $(".midMain").click(function() {
         $(".topMain").css("background-color", "")
-        $(".topMain").css("color", "black")
         $(".jungleMain").css("background-color", "")
-        $(".jungleMain").css("color", "black")
-        $(".midMain").css("background-color", "#A1D7DA")
-        $(".midMain").css("color", "#004f67ff")
+        $(".midMain").css("background-color", "#EDC164")
         $(".adcMain").css("background-color", "")
-        $(".adcMain").css("color", "black")
         $(".supportMain").css("background-color", "")
-        $(".supportMain").css("color", "black")
     });
     $(".adcMain").click(function() {
         $(".topMain").css("background-color", "")
-        $(".topMain").css("color", "black")
         $(".jungleMain").css("background-color", "")
-        $(".jungleMain").css("color", "black")
         $(".midMain").css("background-color", "")
-        $(".midMain").css("color", "black")
-        $(".adcMain").css("background-color", "#A1D7DA")
-        $(".adcMain").css("color", "#004f67ff")
+        $(".adcMain").css("background-color", "#EDC164")
         $(".supportMain").css("background-color", "")
-        $(".supportMain").css("color", "black")
     });
     $(".supportMain").click(function() {
         $(".topMain").css("background-color", "")
-        $(".topMain").css("color", "black")
         $(".jungleMain").css("background-color", "")
-        $(".jungleMain").css("color", "black")
         $(".midMain").css("background-color", "")
-        $(".midMain").css("color", "black")
         $(".adcMain").css("background-color", "")
-        $(".adcMain").css("color", "black")
-        $(".supportMain").css("background-color", "#A1D7DA")
-        $(".supportMain").css("color", "#004f67ff")
+        $(".supportMain").css("background-color", "#EDC164")
     });
 
     // Secondary positions
 
     $(".topSub").click(function() {
-        $(".topSub").css("background-color", "#A1D7DA")
+        $(".topSub").css("background-color", "#EDC164")
         $(".topSub").val(0)
-        $(".topSub").css("color", "#004f67ff")
         $(".jungleSub").css("background-color", "")
         $(".jungleSub").val(1)
-        $(".jungleSub").css("color", "black")
         $(".midSub").css("background-color", "")
         $(".midSub").val(0)
-        $(".midSub").css("color", "black")
         $(".adcSub").css("background-color", "")
         $(".adcSub").val(0)
-        $(".adcSub").css("color", "black")
         $(".supportSub").css("background-color", "")
         $(".supportSub").val(0)
-        $(".supportSub").css("color", "black")
     });
     $(".jungleSub").click(function() {
         $(".topSub").css("background-color", "")
         $(".topSub").val(0)
-        $(".topSub").css("color", "black")
-        $(".jungleSub").css("background-color", "#A1D7DA")
+        $(".jungleSub").css("background-color", "#EDC164")
         $(".jungleSub").val(1)
-        $(".jungleSub").css("color", "#004f67ff")
         $(".midSub").css("background-color", "")
         $(".midSub").val(0)
-        $(".midSub").css("color", "black")
         $(".adcSub").css("background-color", "")
         $(".adcSub").val(0)
-        $(".adcSub").css("color", "black")
         $(".supportSub").css("background-color", "")
         $(".supportSub").val(0)
-        $(".supportSub").css("color", "black")
     });
     $(".midSub").click(function() {
         $(".topSub").css("background-color", "")
-        $(".topMain").val(0)
-        $(".topMain").css("color", "black")
+        $(".topSub").val(0)
         $(".jungleSub").css("background-color", "")
         $(".jungleSub").val(0)
-        $(".jungleSub").css("color", "black")
-        $(".midSub").css("background-color", "#A1D7DA")
+        $(".midSub").css("background-color", "#EDC164")
         $(".midSub").val(0)
-        $(".midSub").css("color", "#004f67ff")
         $(".adcSub").css("background-color", "")
         $(".adcSub").val(0)
-        $(".adcSub").css("color", "black")
         $(".supportSub").css("background-color", "")
         $(".supportSub").val(0)
-        $(".supportSub").css("color", "black")
     });
     $(".adcSub").click(function() {
         $(".topSub").css("background-color", "")
-        $(".topSub").css("color", "black")
         $(".jungleSub").css("background-color", "")
-        $(".jungleSub").css("color", "black")
         $(".midSub").css("background-color", "")
-        $(".midSub").css("color", "black")
-        $(".adcSub").css("background-color", "#A1D7DA")
-        $(".adcSub").css("color", "#004f67ff")
+        $(".adcSub").css("background-color", "#EDC164")
         $(".supportSub").css("background-color", "")
-        $(".supportSub").css("color", "black")
     });
     $(".supportSub").click(function() {
         $(".topSub").css("background-color", "")
-        $(".topSub").css("color", "black")
         $(".jungleSub").css("background-color", "")
-        $(".jungleSub").css("color", "black")
         $(".midSub").css("background-color", "")
-        $(".midSub").css("color", "black")
         $(".adcSub").css("background-color", "")
-        $(".adcSub").css("color", "black")
-        $(".supportSub").css("background-color", "#A1D7DA")
-        $(".supportSub").css("color", "#004f67ff")
+        $(".supportSub").css("background-color", "#EDC164")
     });
 
     // This is the smash bros side of the js
@@ -278,29 +307,6 @@ $(document).ready(function() {
         $(".signing").text(signature);
     });
 
-    // Cost incrementer
-
-    $("")
-
-    // when user clicks on "Free agent"
-    $(".free").click(function() {
-        $("#freeHidden").slideDown();
-        $(".team_choice").slideUp();
-        $(".cont_newteam").slideUp();
-    });
-
-    // when user clicks on "Already Have a Team"
-    $(".old").click(function() {
-        $(".team_choice").slideDown();
-        $("#freeHidden").slideUp();
-        $(".cont_newteam").slideUp();
-    });
-
-    // when user clicks on "Add New Team"
-    $(".add").click(function() {
-        $(".cont_newteam").slideDown();
-        $("#freeHidden").slideUp();
-        $(".team_choice").slideUp();
-    });
+    
     
 });
